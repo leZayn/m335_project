8c19c6bf67f430a548f04db10ad66dce0bffe1cf
Severin <severin.baur@noseryoung.ch>
2021-05-20 16:40:21 +0200

    Activity

--- Penz/app/src/main/java/m335/penz/MainActivity.java
+++ Penz/app/src/main/java/m335/penz/MainActivity.java
@@ -2,13 +2,27 @@ package m335.penz;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Button createButton;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        createButton = findViewById(R.id.createButton);
+        createButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
+   /* private void openActivityCreatePendenz(){
+        Intent intent = new Intent(this, )
+    }*/
 }
