7c6ee0e7fcbaec5836751139e1cd91d9fb61ef3c
phearum <svay.phearum@gmail.com>
2021-05-26 14:14:06 +0200

    Added datepicker and dropdown menu

--- Penz/app/src/main/java/m335/penz/CreateActivity.java
+++ Penz/app/src/main/java/m335/penz/CreateActivity.java
@@ -3,49 +3,53 @@ package m335.penz;
 import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.text.InputType;
-import android.view.View;
-import android.widget.DatePicker;
+import android.widget.ArrayAdapter;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.material.datepicker.MaterialDatePicker;
+import com.google.android.material.datepicker.MaterialPickerOnPositiveButtonClickListener;
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
 
-import java.util.Calendar;
+import java.util.Arrays;
+import java.util.List;
 
 import m335.penz.persistence.PendencyDao;
 
 public class CreateActivity extends AppCompatActivity {
 
-/*   private PendencyDao pendencyDao;
+    private PendencyDao pendencyDao;
 
     private DatePickerDialog datePickerDialog;
-    private TextInputEditText textInputEditText;
+    private TextInputEditText textInputEditTextCalendar;
+    private TextInputLayout textInputLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create);
-        textInputEditText = findViewById(R.id.btn_datePicker);
-        textInputEditText.setInputType(InputType.TYPE_NULL);
-        textInputEditText.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                System.out.println("--------------- CLICK ---------------");
-                final Calendar calendar = Calendar.getInstance();
-                int day = calendar.get(Calendar.DAY_OF_MONTH);
-                int month = calendar.get(Calendar.MONTH);
-                int year = calendar.get(Calendar.YEAR);
-                datePickerDialog = new DatePickerDialog(CreateActivity.this, new DatePickerDialog.OnDateSetListener() {
-                    @Override
-                    public void onDateSet(DatePicker datePicker, int year, int month, int day) {
-                        textInputEditText.setText(day + "." + (month + 1) + "." + year);
-                    }
-                }, year, month, day);
-
-                datePickerDialog.show();
-
-                System.out.println("--------------- CLICK2 ---------------");
-            }
+        textInputEditTextCalendar = (TextInputEditText)findViewById(R.id.tE_datePicker);
+        textInputEditTextCalendar.setInputType(InputType.TYPE_NULL);
+        textInputEditTextCalendar.setOnClickListener(view -> {
+            System.out.println("--------------- CLICK ---------------");
+            MaterialDatePicker datePicker =
+                    MaterialDatePicker.Builder.datePicker()
+                            .setTitleText("Select date")
+                            .setSelection(MaterialDatePicker.todayInUtcMilliseconds())
+                            .build();
+            datePicker.addOnPositiveButtonClickListener(new MaterialPickerOnPositiveButtonClickListener() {
+                @Override
+                public void onPositiveButtonClick(Object selection) {
+                    textInputEditTextCalendar.setText(datePicker.getHeaderText());
+                }
+            });
+            datePicker.show(getSupportFragmentManager(), "tag");
+            System.out.println("--------------- CLICK2 ---------------");
         });
-    }*/
+
+        List<String> items = Arrays.asList("Option 1", "Option 2", "Option 3", "Option 4");
+        ArrayAdapter adapter = ArrayAdapter(requireContext(), R.layout.activity_create, items);
+        (textInputLayout.getEditText() as? AutoCompleteTextView)?.setAdapter(adapter)
+    }
 }
